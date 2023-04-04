659282d63b5f6f16e34de4742a71aef04a6a1f42
Scarpato-Jacopo <71266271+Scarpato-Jacopo@users.noreply.github.com>
2023-04-04 19:24:00 +0200

    Delete codice/bsbarilla directory

--- codice/bsbarilla/scripts/diventaFatturatore.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$nome = $_REQUEST['nomeFatt'];
-    $pass = $_REQUEST['passFatt'];
-    if($nome == "admin" && $pass == "bsbarilla"){
-    	header("Location: ../index.php?isFatturatore=1");
-    }
-    else header("Location: ../index.php?isFatturatore=0");
-?>
--- codice/bsbarilla/scripts/down.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-    $_SESSION['down'] = 1;
-    sleep(180);
-    $_SESSION['down'] = 0; 
-?>
